f3bc49db3946a5a675b8cefa475cf369d0688a9c
Long Pham <longkul1478@gmail.com>
2023-11-29 00:11:55 +0700

    update

--- pages/movies/[id]/index.vue
+++ pages/movies/[id]/index.vue
@@ -96,67 +96,6 @@ const actionSlide = (status: boolean): void => {
         </div>
     </div>
     <div class="overflow-x-hidden">
-        <carousel :autoplay="3000" :wrap-around="true" class="relative slideshow" ref="slideshow">
-            <slide v-for="(item, index) in pictures" :key="index">
-                <img :src="item.src" :alt="item.alt" class="carousel-image" />
-            </slide>
-            <template #addons>
-                <navigation />
-                <pagination class="sm:block hidden" />
-            </template>
-        </carousel>
-        <div
-            class="relative bg-[url('/images/pattern.png')] bg-repeat bg-top max-w-full flex flex-wrap items-center justify-center">
-            <div class="relative w-full">
-                <img src="/images/background-header.png" alt="Top header">
-            </div>
-            <div class="flex flex-wrap lg:gap-x-32 gap-5 justify-center items-center py-5">
-                <div class="flex items-center justify-between gap-x-10 relative">
-                    <div class="w-[114px] h-[130px] sm:block hidden">
-                        <img class="absolute -top-10 left-0" :src="useAsset('images/home/icon-ticket.png')"
-                            alt="ticket banner">
-                    </div>
-                    <h2
-                        class="uppercase font-bold text-[#e00d7a] leading-[32px] text-[32px] text-shadow-xs text-shadow font-Futurab">
-                        mua vé <br>
-                        online
-                    </h2>
-                </div>
-                <div class="grid grid-cols-2 gap-5">
-                    <select-search model-value=""
-                        class="rounded-tr-[24px] rounded-br-[24px] rounded-bl-[24px] sm:w-[300px] w-[45vw] py-[10px] border-0 uppercase"
-                        :options="[
-                            { label: 'Chuyến xe cuối cùng', value: '1' },
-                            { label: 'Shin - Cậu bé bút chì', value: '2' },
-                            { label: 'Doraemon - Xứ sở thần tiên', value: '3' },
-                        ]" placeholder="Chọn phim" />
-                    <select-search model-value=""
-                        class="rounded-tl-[24px] rounded-bl-[24px] rounded-br-[24px] sm:w-[300px] w-[45vw] py-[10px] border-0 uppercase"
-                        :options="[
-                            { label: 'Chuyến xe cuối cùng', value: '1' },
-                            { label: 'Shin - Cậu bé bút chì', value: '2' },
-                            { label: 'Doraemon - Xứ sở thần tiên', value: '3' },
-                        ]" placeholder="Chọn rạp" />
-                    <select-search model-value=""
-                        class="rounded-tl-[24px] rounded-tr-[24px] rounded-br-[24px] sm:w-[300px] w-[45vw] py-[10px] border-0 uppercase"
-                        :options="[
-                            { label: 'Chuyến xe cuối cùng', value: '1' },
-                            { label: 'Shin - Cậu bé bút chì', value: '2' },
-                            { label: 'Doraemon - Xứ sở thần tiên', value: '3' },
-                        ]" placeholder="Chọn ngày" />
-                    <select-search model-value=""
-                        class="rounded-tl-[24px] rounded-tr-[24px] rounded-bl-[24px] sm:w-[300px] w-[45vw] py-[10px] border-0 uppercase"
-                        :options="[
-                            { label: 'Chuyến xe cuối cùng', value: '1' },
-                            { label: 'Shin - Cậu bé bút chì', value: '2' },
-                            { label: 'Doraemon - Xứ sở thần tiên', value: '3' },
-                        ]" placeholder="Chọn suất chiếu" />
-                </div>
-            </div>
-            <div>
-
-            </div>
-        </div>
         <div class="px-[10%] bg-[url('/public/images/film-bg.jpg')] pt-5 pb-5">
           <div class="flex items-center justify-center gap-5">
             <div class="border-[9px] border-solid border-[#fae2a2] w-[40%] h-full">
@@ -237,7 +176,6 @@ const actionSlide = (status: boolean): void => {
           </div>
         </div>
 
-
         <div class="bg-gradient-to-tr from-[#4E0045] to-[#410434]">
             <h1 class="text-center text-[30px] py-[40px] uppercase text-white font-Futurab">Phim hay trong tuần</h1>
             <div class="relative">
@@ -256,67 +194,7 @@ const actionSlide = (status: boolean): void => {
                     <CarouselProduct v-model:model-value="slideshowCurrent" :data="dangChieu" />
                 </div>
             </div>
-            <div class="flex flex-col items-center justify-center">
-                <!-- ưu đãi -->
-                <div class="w-[70%] flex gap-6 my-10 items-center">
-                    <div class="flex flex-col w-24 h-24">
-                        <h3 class="text-white uppercase font-avantgarde-demi text-3xl">
-                            ƯU ĐÃI
-                        </h3>
-                        <img class="w-24 h-24" :src="useAsset('images/home/icon-promotion.png')" alt="promotion">
-                    </div>
-                    <div>
-                        <carousel class="slideshow" :autoplay="3000" :wrapAround="true" :itemsToShow="3">
-                            <Slide v-for="slide in 10" :key="slide">
-                                <div
-                                    class="p-2 bg-white border-[5px] rounded-tl-3xl rounded-br-3xl border-[#723369] shadow-[7px 7px 0 rgba(0,0,0,0.2)]">
-                                    <img src="/images/c_monday.jpg" alt=""
-                                        class="w-120 h-120 rounded-lg object-cover mr-[16px]">
-                                </div>
-                            </Slide>
-                        </carousel>
-                    </div>
-                </div>
-                <!-- tin tức -->
-                <div class="w-[70%] flex gap-6 my-10 items-center">
-                    <div class="flex flex-col w-24 h-24">
-                        <h3 class="text-white uppercase font-avantgarde-demi text-3xl">
-                            Tin tức
-                        </h3>
-                        <img class="w-24 h-24" :src="useAsset('images/home/icon-news.png')" alt="promotion">
-                    </div>
-                    <div class="grid grid-cols-2 gap-2">
-                        <div class="grid grid-cols-2 p-4 bg-[#e00d7a] gap-2 rounded-tr-3xl rounded-bl-3xl">
-                            <div class="text-white font-MyriadRegular">
-                                <h1 class="text-base mb-2">
-                                    Khai trương chi nhánh mới khuyến mãi 100% cho các vé xem phim
-                                </h1>
-                                <p class="text-sm">
-                                    Cùng trở thành nhân viên rạp phim để được làm việc trong môi trường chuyên nghiệp. Đến
-                                    ngay
-                                    Cinestar
-                                    Vietnam để trao cho mình cơ hội tuyển dụng đặc biệt vào 13.12.2022 nhé!
-                                </p>
-                            </div>
-                            <div class="w-full h-full">
-                                <img src="/images/c_monday.jpg" alt=""
-                                    class="w-full h-full rounded-tr-3xl object-cover mr-[16px]">
-                            </div>
-                        </div>
-                        <div>
-                            <carousel class="slideshow" :autoplay="3000" :wrapAround="true" :itemsToShow="3">
-                                <Slide v-for="slide in 10" :key="slide">
-                                    <div
-                                        class="p-2 bg-white border-[5px] rounded-tl-3xl rounded-br-3xl border-[#723369] shadow-[7px 7px 0 rgba(0,0,0,0.2)]">
-                                        <img src="/images/c_monday.jpg" alt=""
-                                            class="w-120 h-120 rounded-lg object-cover mr-[16px]">
-                                    </div>
-                                </Slide>
-                            </carousel>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <NewsAndPromotion />
         </div>
     </div>
 </template>
