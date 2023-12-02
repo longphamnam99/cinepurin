0ef19f9f057213746bd85de54e5b1ebe18a1db80
Long Pham <longkul1478@gmail.com>
2023-12-02 18:08:00 +0700

    update

--- components/Header.vue
+++ components/Header.vue
@@ -5,6 +5,14 @@ import {useAuthStore} from '~/stores/auth'
 import Cookies from 'js-cookie'
 import Directors from 'pages/manager/directors.vue';
 
+interface IProps {
+  showBannerAndSearch?: boolean
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  showBannerAndSearch: true,
+})
+
 const router = [
   {
     path: "/movies",
@@ -45,8 +53,8 @@ const showModal = () => {
 }
 
 const form = ref({
-  username: null,
-  password: null
+  taiKhoan: null,
+  matKhau: null
 })
 
 const formRegister = ref({
@@ -72,33 +80,29 @@ watch(token, (newToken) => {
   immediate: true,
 })
 
+const userData = ref(Cookies.get('userData'))
+
 const submitForm = async () => {
   const data = await useApiBridge({
-    url: "auth/signin",
+    url: "QuanLyNguoiDung/DangNhap",
     method: "post",
     data: form.value
   })
 
-  if (data.code === 200) {
-    Cookies.set('token', data.data.token)
+  if (data.accessToken) {
+    Cookies.set('token', data.accessToken)
+    Cookies.set('userData', JSON.stringify(data))
+
+    userData.value = Cookies.get('userData')
     isShowLogin.value = false
     isShowModalLogin.value = false
   }
 }
 
 const logout = async () => {
-  const data = await useApiBridge({
-    url: "auth/logout",
-    method: "post",
-    data: {
-      data: null
-    },
-    useToken: true
-  })
-  if (data.code === 200) {
-    Cookies.set('token', "")
-    isShowLogin.value = true
-  }
+  Cookies.set('token', '')
+  Cookies.set('userData', '')
+  isShowLogin.value = true
 }
 
 const register = async () => {
@@ -166,22 +170,28 @@ const isOpen = ref(false)
     <div class="bg-[url('/images/pattern.png')] bg-repeat bg-top max-w-full flex items-center justify-center">
       <div class="lg:w-[90%] w-full items-center flex flex-wrap lg:justify-between justify-center gap-x-10">
         <div class="flex items-center sm:gap-x-10 gap-x-8">
-          <div v-if="isShowLogin" class="flex items-center gap-x-2 cursor-pointer" @click="isShowModalRegister = true">
+          <div class="flex items-center gap-x-2 cursor-pointer" @click="isShowModalRegister = true">
             <img class="sm:w-auto sm:h-auto w-[30px]" src="/images/icon-register.png" alt="icon register">
-            <nuxt-link to="/register">
-              <p class="uppercase text-[#e00d7a] font-bold sm:text-base text-xs font-avantgarde-demi">
+            <span class="cursor-pointer" @click="useRouter().push('/register')">
+              <span v-if="isShowLogin"
+                    class="uppercase text-[#e00d7a] font-bold sm:text-base text-xs font-avantgarde-demi">
                 Đăng ký thành viên
-              </p>
-            </nuxt-link>
+              </span>
+              <span v-else class="uppercase text-[#e00d7a] font-bold sm:text-base text-xs font-avantgarde-demi">
+                                           {{ JSON.parse(userData)?.hoTen }}
+              </span>
+            </span>
           </div>
-          <div v-if="isShowLogin" class="flex items-center gap-x-2 cursor-pointer" @click="isShowModalLogin = true">
+          <div class="flex items-center gap-x-2 cursor-pointer" @click="isShowModalLogin = true">
             <img class="sm:w-auto sm:h-auto w-[30px]" src="/images/icon-login.png" alt="icon login">
-            <p class="uppercase text-[#e00d7a] font-bold sm:text-base text-xs font-avantgarde-demi">
+            <span v-if="isShowLogin"
+                  class="uppercase text-[#e00d7a] font-bold sm:text-base text-xs font-avantgarde-demi">
               Đăng nhập
-            </p>
-          </div>
-          <div v-else class="cursor-pointer" @click="logout">
-            Đăng xuất
+            </span>
+            <span v-else @click="logout" class="uppercase text-[#e00d7a] font-bold sm:text-base text-xs
+              font-avantgarde-demi">
+              Đăng xuất
+            </span>
           </div>
         </div>
         <div class="flex items-center">
@@ -195,7 +205,7 @@ const isOpen = ref(false)
       </div>
     </div>
   </nav>
-  <SlideAndSearch/>
+  <SlideAndSearch v-if="showBannerAndSearch"/>
   <teleport to="body">
     <!--đăng nhập -->
 
@@ -211,13 +221,13 @@ const isOpen = ref(false)
           <div class="relative z-0 w-full mb-6 group">
             <div class="relative">
               <!-- Input với hình ảnh nền -->
-              <input v-model="form.username" type="text" name="floating_email" id="floating_email"
+              <input v-model="form.taiKhoan" type="text" name="floating_email" id="floating_email"
                      class="w-full pl-10 pr-4 py-2 border-1 border-solid border-rose-500 rounded-ss-[30px] rounded-se-[30px] rounded-ee-[30px] rounded-es-[30px]"
                      placeholder=" " required/>
 
               <!-- Hình ảnh nền cho input -->
               <div class="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
-                <img src="../images/icon-user.png" alt="Hình ảnh" class="h-6 w-6"/>
+                <img src="/images/icon-user.png" alt="Hình ảnh" class="h-6 w-6"/>
               </div>
             </div>
           </div>
@@ -225,13 +235,13 @@ const isOpen = ref(false)
           <div class="relative z-0 w-full mb-6 group">
             <div class="relative">
               <!-- Input với hình ảnh nền -->
-              <input v-model="form.username" type="text" name="floating_email" id="floating_email"
+              <input v-model="form.matKhau" type="password" name="floating_email" id="floating_email"
                      class="w-full pl-10 pr-4 py-2 border-1 border-solid border-rose-500 rounded-ss-[30px] rounded-se-[30px] rounded-ee-[30px] rounded-es-[30px]"
                      placeholder=" " required/>
 
               <!-- Hình ảnh nền cho input -->
               <div class="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
-                <img src="../images/icon-pass.png" alt="Hình ảnh" class="h-6 w-6"/>
+                <img src="/images/icon-pass.png" alt="Hình ảnh" class="h-6 w-6"/>
               </div>
             </div>
           </div>
@@ -242,16 +252,8 @@ const isOpen = ref(false)
                 ĐĂNG NHẬP
               </button>
             </div>
-            <div class="flex">
-              <button type="submit"
-                      class="bg-wwhite px-[31px] py-[15px] text-[#c0bebe] font-avantgarde-demi text-[16px] pr-[10px] text-right">
-                QUÊN MẬT KHẨU
-              </button>
-            </div>
-
           </div>
         </form>
-
       </template>
     </Modal>
   </teleport>
